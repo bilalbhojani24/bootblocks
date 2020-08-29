45fb135977095bc124517fe3919cc1b6e8bac40d
Bilal Bhojani <61111577+bilalbhojani24@users.noreply.github.com>
2020-08-28 22:16:53 -0700

    linking and improvisation in UI

---
+++ assets/js/navigationbar.js
@@ -0,0 +1,40 @@
+const header_a = document.getElementById("headera").innerHTML;
+const header_b = document.getElementById("headerb").innerHTML;
+
+function preview(code_text, blog_name, but_name) {
+    var code_div = document.createElement('div');
+    code_div.classList.add("code_div");
+    code_div.innerText = code_text;
+    document.getElementById(blog_name).append(code_div);
+    document.getElementById(but_name).disabled = true;
+    code_div.setAttribute("id", "code_div")
+}
+function copyToClipboard(code_text, but_name) {
+    const el = document.createElement('textarea');
+    el.value = code_text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    document.getElementById(but_name).disabled = false;
+    document.getElementById("code_div").remove();
+}
+
+
+document.getElementById("pre1").addEventListener('click', function () {
+    console.log("clicked");
+    preview(header_a, "headera", "pre1");
+});
+document.getElementById("ctc1").addEventListener('click', function () {
+    copyToClipboard(header_a, "pre1");
+});
+
+
+document.getElementById("pre2").addEventListener('click', function () {
+    console.log("clicked");
+    preview(header_b, "headerb", "pre2");
+});
+document.getElementById("ctc2").addEventListener('click', function () {
+    copyToClipboard(header_b, "pre2");
+});
+
